0a6a9ab10561d8811d4b036333e3330b3ad46729
Jared <jared@Jareds-MacBook-Pro.local>
2022-10-01 21:24:51 -0500

    first

---
+++ social/accounts/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+class UserCreateForm(UserCreationForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = {'username','email','password1','password2'}
+        
+
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.fields['email'].label = "Email Address"
+        self.fields['password1'].label = "Create Password"
+        self.fields['username'].label = "Display Name"
+        self.fields['password2'].label = "Verify Password"
+
+
+
